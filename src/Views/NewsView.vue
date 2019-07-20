5ce976503cc92f34494a4b5186d943d5db20d73a
soyoung <soyoungp02@gmail.com>
2019-07-21 00:37:52 +0900

    axios 이용하여 api 호출

--- src/Views/NewsView.vue
+++ src/Views/NewsView.vue
@@ -1,12 +1,30 @@
 <template>
     <div>
-        news
+        <div v-for="item in list">{{ item }}</div>
     </div>
 </template>
 
 <script>
-export default {
+import axios from 'axios';
 
+export default {
+    data() {
+        return {
+            list: [],
+        }
+    },
+
+    created() {
+        const vm = this;
+        axios.get("https://api.hnpwa.com/v0/news/1.json")
+        .then(function(response) {
+            console.log(response);
+            vm.list = response.data;
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
+    }
 }
 </script>
 
