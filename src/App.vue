4593931979c40513fdf1debd387a4935fed02dd9
soyoung <soyoungp02@gmail.com>
2019-07-20 22:43:56 +0900

    리다이렉트 및 툴바 컴포넌트 추가 후 링크 연결

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
-  <router-view></router-view>
+  <div id="app">
+    <tool-bar></tool-bar>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
+import ToolBar from './components/ToolBar.vue';
 
 export default {
+  components: {
+    ToolBar,
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
