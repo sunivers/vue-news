ba0ed02ea0c0eedf0a94e696d6ccdf8ba99a4663
soyoung <soyoungp02@gmail.com>
2019-07-25 00:44:19 +0900

    map 헬퍼를 이용하여 state값을 바인딩하는 다양한 방법 적용

--- src/Views/AskView.vue
+++ src/Views/AskView.vue
@@ -8,6 +8,10 @@
 import { mapState, mapGetters } from 'vuex';
 export default {
     computed: {
+        //this.$store.state.ask 문자열 길이를 줄이기 위한 다양한 데이터 바인딩 방법.
+        //개인적으로는 2번 방법을 픽. 왜냐하면 가장 공식적이고 역할에 맞는 방식인것 같다.
+        //computed와 getters는 본래 계산된 속성을 사용하기 위해 존재하므로 단순히 리턴만 해주는 경우는 역할에 맞지 않는다. 
+
         // #3
         ...mapGetters({
             fetchedAsk: 'fetchedAsk',
