3b95d8753807e6e0ae78772d3f493b74b149c03f
soyoung <soyoungp02@gmail.com>
2019-07-25 00:46:44 +0900

    스토어 속성 모듈화 (mutations, actions 분리)

---
+++ src/store/actions.js
@@ -0,0 +1,25 @@
+import { fetchNewsList, fetchJobsList, fetchAskList } from '../api/index.js';
+
+export default {
+    //context.commit을 destructuring(구조 분해 할당)하여 commit값만 가져온다.
+    FETCH_NEWS({ commit }) {
+        fetchNewsList()
+        //위와 마찬가지로 response.data를 구조 분해 할당하여 data값만 가져온다.
+        .then(({ data }) => {
+            commit('SET_NEWS', data);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    },
+    FETCH_JOBS(context) {
+        fetchJobsList()
+        .then(response => context.commit('SET_JOBS', response.data))
+        .catch(error => console.log(error))
+    },
+    FETCH_ASK(context) {
+        fetchAskList()
+        .then(response => context.commit('SET_ASK', response.data))
+        .catch(error => console.log(error))
+    }
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { fetchNewsList, fetchJobsList, fetchAskList } from '../api/index.js';
+import mutations from './mutations.js';
+import actions from './actions.js';
 
 Vue.use(Vuex);
 
@@ -15,38 +16,6 @@ export const store = new Vuex.Store({
             return state.ask;
         }
     },
-    mutations: {
-        SET_NEWS(state, news) {
-            state.news = news;
-        },
-        SET_JOBS(state, jobs) {
-            state.jobs = jobs;
-        },
-        SET_ASK(state, ask) {
-            state.ask = ask;
-        }
-    },
-    actions: {
-        //context.commit을 destructuring(구조 분해 할당)하여 commit값만 가져온다.
-        FETCH_NEWS({ commit }) {
-            fetchNewsList()
-            //위와 마찬가지로 response.data를 구조 분해 할당하여 data값만 가져온다.
-            .then(({ data }) => {
-                commit('SET_NEWS', data);
-            })
-            .catch(error => {
-                console.log(error);
-            })
-        },
-        FETCH_JOBS(context) {
-            fetchJobsList()
-            .then(response => context.commit('SET_JOBS', response.data))
-            .catch(error => console.log(error))
-        },
-        FETCH_ASK(context) {
-            fetchAskList()
-            .then(response => context.commit('SET_ASK', response.data))
-            .catch(error => console.log(error))
-        }
-    }
+    mutations: mutations,
+    actions //축약형
 });
