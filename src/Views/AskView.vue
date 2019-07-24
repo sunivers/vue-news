dc89ad13d9fedacbca1eca75f4e2da233fb551c8
soyoung <soyoungp02@gmail.com>
2019-07-25 00:40:20 +0900

    map 헬퍼를 이용하여 state값을 바인딩하는 다양한 방법 적용

--- src/Views/AskView.vue
+++ src/Views/AskView.vue
@@ -1,11 +1,28 @@
 <template>
     <div>
-        <div v-for="(item, i) in this.$store.state.ask" :key="i">{{ item.title }}</div>
+        <div v-for="(item, i) in fetchedAsk" :key="i">{{ item.title }}</div>
     </div>
 </template>
 
 <script>
+import { mapState, mapGetters } from 'vuex';
 export default {
+    computed: {
+        // #3
+        ...mapGetters({
+            fetchedAsk: 'fetchedAsk',
+        })
+
+        // #2
+        // ...mapState({
+        //     ask: state => state.ask,
+        // }),
+
+        // #1
+        // ask() {
+        //     return this.$store.state.ask;
+        // }
+    },
     created() {
         this.$store.dispatch('FETCH_ASK');
 
